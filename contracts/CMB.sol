d6013eaef3ca78fd4d7634235b5fc115f99d6794
Trương Tiến Nhật <nhat.truongdn98@gmail.com>
2022-05-09 16:35:02 +0700

    add service fee to contract

--- contracts/CMB.sol
+++ contracts/CMB.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CMB {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CMB is Ownable {
     struct Payment {
         address bo;
         address client;
@@ -13,7 +15,7 @@ contract CMB {
     /*
      * Status:
      * After BO creates payment: INITIAL,
-     * After Client escrows money: ESCROWED,
+     * After Client escrows money: PAID,
      * After Client confirm to release money: CONFIRMED
      * After BO release money: RELEASED
      * After Client withdraw money: FINISHED
@@ -21,18 +23,21 @@ contract CMB {
     enum Status { INITIAL, PAID, CONFIRMED, RELEASED, FINISHED }
 
     mapping(uint256 => Payment) public payments;
+    uint256 public serviceFeeTotal;
+    uint256 public serviceFee;
 
     event RequestPayment(uint256 indexed paymentId, address indexed bo, address indexed client, bytes32 data, uint256 amount);
     event Pay(uint256 indexed paymentId);
     event ConfirmToRelease(uint256 indexed paymentId);
     event Claim(uint256 indexed paymentId);
+    event WithdrawServiceFee(uint256 amount, address indexed fundingReceiver);
 
-    modifier onlyValidAddress(address _addr) {
+    modifier onlyValidAddress(address _address) {
         uint32 size;
         assembly {
-            size := extcodesize(_addr)
+            size := extcodesize(_address)
         }
-        require((size <= 0) && _addr != address(0), "Invalid address");
+        require((size <= 0) && _address != address(0), "Invalid address");
         _;
     }
 
@@ -46,29 +51,37 @@ contract CMB {
         _;
     }
 
-    modifier onlyUserInPayment(uint256 paymentId) {
-        require(
-            msg.sender == payments[paymentId].bo || msg.sender == payments[paymentId].client, 
-            "Only Client can do it"
-        );
-        _;
-    }
-
     modifier onlyOnInitialPayment(uint256 paymentId) {
         require(payments[paymentId].status == Status.INITIAL, "This payment needs to initialized");
         _;
     }
 
+    constructor() {
+        serviceFee = 0.02 ether;
+    }
+
     /** 
      *  @notice Get amount of payment by payment ID
      * 
-     *  @dev    Only user in payment can call this function. 
+     *  @dev    Anyone can call this function. 
      * 
      *          Name        Meaning 
      *  @param  paymentId   ID of payment that needs to get fee 
      */ 
-    function fee(uint256 paymentId) external view onlyUserInPayment(paymentId) returns (uint256) {
+    function getPaymentAmount(uint256 paymentId) external view returns (uint256) {
         return payments[paymentId].amount;
+    }
+
+    /** 
+     *  @notice Set service fee
+     * 
+     *  @dev    Only owner can call this function. 
+     * 
+     *          Name        Meaning 
+     *  @param  _amount     Amount of service fee that want to update 
+     */ 
+    function setServiceFee(uint256 _amount) external onlyOwner {
+        serviceFee = _amount;
     }
 
     /** 
@@ -123,6 +136,7 @@ contract CMB {
             msg.sender != client, 
             "Business Owner and Client can not be same"
         );
+        require(amount > 0, "Amount must be greater than 0");
 
         payments[paymentId] = Payment(msg.sender, client, data, amount, Status.INITIAL);
         emit RequestPayment(paymentId, msg.sender, client, data, amount);
@@ -138,11 +152,12 @@ contract CMB {
      */
     function pay(uint256 paymentId) external payable onlyClient(paymentId) onlyOnInitialPayment(paymentId) {
         require(
-            msg.value == payments[paymentId].amount, 
+            msg.value == payments[paymentId].amount + serviceFee, 
             "Not enough fee according to payment"
         );
 
         payments[paymentId].status = Status.PAID;
+        serviceFeeTotal += serviceFee;
         emit Pay(paymentId);
     }
 
@@ -172,8 +187,25 @@ contract CMB {
     function claim(uint256 paymentId) external payable onlyBusinessOwner(paymentId) {
         require(payments[paymentId].status == Status.CONFIRMED, "This payment needs to confirmed by client");
 
-        payable(msg.sender).transfer(payments[paymentId].amount);
         payments[paymentId].status = Status.RELEASED;
+        payable(msg.sender).transfer(payments[paymentId].amount);
         emit Claim(paymentId);
+    }
+
+    /** 
+     *  @notice Withdraw amount of service fee to specific address
+     * 
+     *  @dev    Only Owner can call this function. 
+     * 
+     *          Name                Meaning 
+     *  @param  _amount             Amount of service fee that want to withdraw
+     *  @param  _fundingReceiver    Address that want to transfer
+     */
+    function withdrawServiceFee(uint256 _amount, address _fundingReceiver) external payable onlyOwner {
+        require(_amount <= serviceFeeTotal, "Not enough to withdraw");
+
+        serviceFeeTotal -= _amount;
+        payable(_fundingReceiver).transfer(_amount);
+        emit WithdrawServiceFee(_amount, _fundingReceiver);
     }
 }
