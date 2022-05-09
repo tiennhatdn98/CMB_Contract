f1beb26e8e7d7cb2ee9f5dfa7a4422ff115f79ce
Trương Tiến Nhật <nhat.truongdn98@gmail.com>
2022-05-09 11:56:04 +0700

    change hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,54 @@
+// Loading env configs for deploying and public contract source
+require("dotenv").config();
+// Using hardhat-ethers plugin for deploying
+// See here: https://hardhat.org/plugins/nomiclabs-hardhat-ethers.html
+// https://hardhat.org/guides/deploying.html
+require("@nomiclabs/hardhat-ethers");
+// Testing plugins with Waffle
+// See here: https://hardhat.org/guides/waffle-testing.html
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
+// This plugin runs solhint on the project's sources and prints the report
+// See here: https://hardhat.org/plugins/nomiclabs-hardhat-solhint.html
+require("@nomiclabs/hardhat-solhint");
+// Verify and public source code on etherscan
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+const config = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: { accounts: { count: 100 } },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [process.env.DEPLOY_ACCOUNT],
+    },
+    mainnet: {
+      url: "https://bsc-dataseed1.ninicoin.io",
+      accounts: [process.env.DEPLOY_ACCOUNT],
+      gasPrice: 8000000000,
+    },
+  },
+  etherscan: { apiKey: process.env.BINANCE_API_KEY },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+    deploy: "deploy",
+    deployments: "deployments",
+  },
+  mocha: {
+    timeout: 200000,
+    useColors: true,
+    reporter: "mocha-multi-reporters",
+    reporterOptions: { configFile: "./mocha-report.json" },
+  },
 };
+module.exports = config;
